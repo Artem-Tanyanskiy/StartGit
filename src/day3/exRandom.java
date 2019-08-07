7904421a041ba1bd0a227fae77ef6591a414eabb
Artem <artem.tanyanskiy.ua@gmail.com>
2019-08-07 11:30:29 +0300

    adding random

---
+++ src/day3/exRandom.java
@@ -0,0 +1,11 @@
+package day3;
+import java.util.*;
+public class exRandom {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int i;
+        i = rand.nextInt(10);
+        System.out.println("i = "+i);
+
+    }
+}
