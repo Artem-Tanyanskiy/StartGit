cf5bb117d69bbecff180a90327b569d7a3c434af
Artem <artem.tanyanskiy.ua@gmail.com>
2019-08-29 11:24:00 +0300

    update

--- src/Algorithms/BubbleSort.java
+++ src/Algorithms/BubbleSort.java
@@ -1,16 +1,17 @@
 package Algorithms;
 
+import java.util.Arrays;
+
 public class BubbleSort {
     public static void main(String[] args) {
-        BubbleSort ob = new BubbleSort();
         int mas[] ={5,6,8,2,4,8,2,5,1,9};
         System.out.println("Default mas");
-        ob.printMas(mas);
-        ob.bubbleSort(mas);
+        System.out.println(Arrays.toString(mas));
+        bubbleSort(mas);
         System.out.println("Sort mas");
-        ob.printMas(mas);
+        System.out.println(Arrays.toString(mas));
     }
-    void bubbleSort(int mas[]){
+   public static void bubbleSort(int mas[]){
         int a;
         for(int i=0; i<mas.length-1; i++){
             for(int j=0;j<mas.length-1-i;j++){
@@ -23,12 +24,6 @@ public class BubbleSort {
         }
 
     }
-    void printMas(int mas[]){
-        for(int i:mas){
-            System.out.print(i+" ");
-            i++;
-        }
-        System.out.println();
-    }
+
 
     }
